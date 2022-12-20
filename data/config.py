89006f2ac54e0b33a80b457ccc1ebf9d5cf7b2c9
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:51:01 -0800

    Update config.py

--- data/config.py
+++ data/config.py
@@ -6,5 +6,7 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
+BOT_ID = BOT_TOKEN.split(:)[1]
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+MYBOT = env.str("MY_BOT")
 IP = env.str("ip")  # Xosting ip manzili
